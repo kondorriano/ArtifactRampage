c2df9447d449194b6f72b0bebeb246bc3e22e30d
franciscofernandez <franciscofernandezmanyas@gmail.com>
2017-07-17 23:12:14 +0200

    Added Dynamite v1 Added Hands for Dynamite v1 Added basic scene for testing Dynamite

---
+++ Assets/Scripts/DynamiteScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamiteScript : MonoBehaviour {
+
+	private float limitTime = 3.0f;
+	private bool activated = false;
+	private bool explode = false;
+	private IEnumerator coroutine;
+
+	public int pId = 1;
+	public TestScript parent = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated) {
+			//TODODODODODOOD:
+			//Particula de la mecha
+		}
+		if (explode) {
+			createExplosion ();
+			activated = false;
+			Destroy (gameObject);
+		}
+	}
+
+	public void ignite() {
+		if (!activated) {
+			activated = true;
+			coroutine = timer ();
+			Debug.Log ("The path is lit.");
+			StartCoroutine (coroutine);
+		}
+	}
+
+	IEnumerator timer() {
+		yield return new WaitForSeconds(limitTime);
+		explode = true;
+	}
+
+	//TODO: crear explosion
+	void createExplosion() {
+		//TODOTODOTODOTODO
+		//Particula explosion
+		//Impactos con entorno
+	}
+
+	void OnDestroy(){
+		if (activated)
+			StopCoroutine (coroutine);
+		if (parent != null && parent.loadedContains (gameObject))
+			parent.loadedRemove (gameObject);
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.CompareTag ("Fire"))
+			ignite ();
+	}
+
+	void OnCollisionEnter(Collision col){
+		DynamiteScript ds= col.gameObject.GetComponent<DynamiteScript> ();
+		if (ds != null && ds.pId == pId)
+			Physics.IgnoreCollision (
+				ds.GetComponent<CapsuleCollider> (),
+				GetComponent<CapsuleCollider> ()
+			);
+	}
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	//Variable para el modelo
+	public GameObject dinamite;
+
+	//Variables de generacion
+	private int ammo = 6;
+	private int totalPos = 4;
+	private List<Transform> positions;
+	private List<GameObject> loaded;
+
+	//Variables de disparo
+	private GameObject actual;
+	private int nextToLit = 3;
+
+	void Start () {
+		positions = new List<Transform> ();
+		loaded = new List<GameObject> ();
+		int i = 0;
+		foreach (Transform child in transform) {
+			positions.Add (child);
+			if (ammo > 0 && i < totalPos) {
+				GameObject t = GameObject.Instantiate (dinamite, child.position, child.rotation, transform);
+				t.GetComponent<Rigidbody> ().useGravity = false;
+				loaded.Add(t);
+				--ammo; ++i;
+			}
+		}
+	}
+
+	void Update () {
+		if (loaded.Count > 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				actual = loaded [loaded.Count-1];
+				actual.transform.parent = null;
+				if (nextToLit > 0)
+					--nextToLit;
+				loaded.RemoveAt (loaded.Count-1);
+				throwDinamite (actual);
+			}
+			if (Input.GetButtonDown ("Fire2")) {
+				actual = loaded [loaded.Count-1];
+				actual.GetComponent<DynamiteScript> ().ignite ();
+				actual.GetComponent<DynamiteScript> ().parent = this;
+			}
+		} else {
+			Debug.Log ("No ammo.");
+		}
+		if (ammo > 0 && loaded.Count < totalPos) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				GameObject t = GameObject.Instantiate (
+					dinamite, 
+					positions [loaded.Count].position, 
+					positions [loaded.Count].rotation, 
+					transform
+				);
+				t.GetComponent<Rigidbody> ().useGravity = false;
+				loaded.Add(t);
+				--ammo;
+			}
+		}
+	}
+
+	private void throwDinamite(GameObject go){
+		go.GetComponent<Rigidbody> ().AddForce (transform.forward * 6.0f, ForceMode.Impulse);
+		go.GetComponent<Rigidbody> ().useGravity = true;
+	}
+
+	public bool loadedContains(GameObject go) {
+		return loaded.Contains (go);
+	}
+		
+	public void loadedRemove(GameObject go){
+		loaded.Remove (go);
+		int i = 0;
+		foreach (GameObject g in loaded) {
+			g.transform.position = positions [i].position;
+			g.transform.rotation = positions [i].rotation;
+			++i;
+		}
+	}
+}
